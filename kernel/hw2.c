dec13cc978c6ed2a0749fbeb24d509914ff83971
Almog Ben-Shaul <37619167+Almogbs@users.noreply.github.com>
2021-12-15 22:51:57 +0200

    Add files via upload

--- kernel/hw2.c
+++ kernel/hw2.c
@@ -11,7 +11,7 @@ int recursiveWeights(struct task_struct *curr) {
     }
 
     list_for_each(iter, &curr->children) {
-        res += recursiveWeights(list_entry(iter, struct task_struct, children));
+        res += recursiveWeights(list_entry(iter, struct task_struct, sibling));
     }
     return res;
 }
@@ -21,33 +21,36 @@ asmlinkage long sys_hello(void) {
     return 0;
 }
 
-asmlinkage int sys_set_weight(int weight) {
-    if (weight < 0) return -EINVAL;
+asmlinkage long sys_set_weight(int weight) {
+    if (weight < 0) {
+        return -EINVAL;
+    }
     current->weight = weight;
     return 0;
 }
 
-asmlinkage int sys_get_weight(void) {
+asmlinkage long sys_get_weight(void) {
     return current->weight;
 }
 
-asmlinkage int sys_get_leaf_children_sum(void) {
-    if (list_empty(&current->children))
+asmlinkage long sys_get_leaf_children_sum(void) {
+    if (list_empty(&current->children)) {
         return -ECHILD;
+    }
 
     return recursiveWeights(current);
 }
 
-asmlinkage pid_t sys_get_heaviest_ancestor(void){
+asmlinkage long sys_get_heaviest_ancestor(void){
     int max_weight = current->weight;
     pid_t max_weight_pid = current->pid;
     struct task_struct* iter = current;
-    while(iter->pid != 1) {
+    while(iter->pid != 0) {
         if (iter->weight > max_weight) {
             max_weight = iter->weight;
             max_weight_pid = iter->pid;
         }
-        iter = iter->real_parent;
+        iter = iter->parent;
     }
     return max_weight_pid;
 }
